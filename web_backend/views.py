7e48a9285980485997b8f76d04c92648445d3609
tprhat <tomislav.prhat@gmail.com>
2024-02-29 18:15:22 +0100

    Create urls for login and signup

--- web_backend/views.py
+++ web_backend/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegistrationForm
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'web_backend/home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/sign_up.html', {"form": form})
