2f34cf8ccebe12392a7357fdc3161b92a04d72fd
tprhat <tomislav.prhat@gmail.com>
2024-03-03 12:57:14 +0100

    Update models

--- web_backend/models.py
+++ web_backend/models.py
@@ -5,21 +5,21 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Transactions(models.Model):
     username = models.ForeignKey(User, models.DO_NOTHING, db_column='username', to_field='username')
-    transaction = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    transaction = models.DecimalField(max_digits=15, decimal_places=2, blank=True, null=True)
+    transaction_name = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = 'transactions'
+    def __str__(self):
+        return self.username.username + ' - ' + str(self.transaction) + ' - ' + self.transaction_name
 
 
 class UserBudget(models.Model):
     username = models.ForeignKey(User, models.DO_NOTHING, db_column='username', to_field='username')
-    budget = models.DecimalField(max_digits=65535, decimal_places=65535)
-
-    class Meta:
-        db_table = 'user_budget'
+    budget = models.DecimalField(max_digits=15, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
