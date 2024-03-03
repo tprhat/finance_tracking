1500edcc83bf92ea6a228a7ce092168d37c24a6c
tprhat <tomislav.prhat@gmail.com>
2024-03-03 12:56:59 +0100

    Login required routes

--- web_backend/views.py
+++ web_backend/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='login')
 def home(request):
     return render(request, 'web_backend/home.html')
 
@@ -19,3 +21,7 @@ def signup(request):
         form = RegistrationForm()
 
     return render(request, 'registration/sign_up.html', {"form": form})
+
+@login_required(login_url='login')
+def add_data(request):
+    return render(request, 'web_backend/add_data.html')
